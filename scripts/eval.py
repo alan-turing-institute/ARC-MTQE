696aed4330a4ee4a2af1c68460ceb549254cfcff
Radka Jersakova <r.jersakova@gmail.com>
2024-02-16 10:50:50 +0000

    add example plots

--- scripts/eval.py
+++ scripts/eval.py
@@ -3,8 +3,9 @@ import os
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-from sklearn.metrics import matthews_corrcoef
+from sklearn.metrics import matthews_corrcoef, precision_recall_curve, roc_curve, RocCurveDisplay, PrecisionRecallDisplay
 
 
 def parse_args():
@@ -43,7 +44,7 @@ def load_data(model, timestamp, lp):
     # merge on sentence indexes
     df_results = pd.merge(df_pred, df_labels, on="idx")
 
-    return df_results
+    return df_results, predictions_dir
 
 
 def main():
@@ -54,21 +55,51 @@ def main():
     language_pairs = ["encs", "ende", "enja", "enzh"]
     for lp in language_pairs:
 
-        df_results = load_data(model, timestamp, lp)
+        df_results, results_path = load_data(model, timestamp, lp)
 
         # higher COMET score --> higher confidence it is NOT an error
         # labels:  ERROR = 0, NOT = 1
         y_true =  np.where(df_results["label"]=="NOT", 1, 0)
+        y_pred = df_results['comet_score']
 
+        # RESULTS
+        # 1. Precision, Recall, FPR, TPR
+        prec, rec, _ = precision_recall_curve(y_true, y_pred)
+        fpr, tpr, _ = roc_curve(y_true, y_pred)
+
+        pr_display = PrecisionRecallDisplay(precision=prec, recall=rec).plot()
+        roc_display = RocCurveDisplay(fpr=fpr, tpr=tpr).plot()
+
+        # 2. MCC
         # tresholds for binarizing COMET output
-        thresholds = np.arange(0.1, 1, 0.1)
+        thresholds = np.arange(0.05, 0.9, 0.05)
+        mccs = []
         for t in thresholds:
-            print("\n", lp)
-
             # the model is treated as "NOT an error" detector
             # i.e., scores above threshold are "NOT an error" predictions
             y_hat = (df_results['comet_score'] >= t).astype(int)
-            print(t, matthews_corrcoef(y_true, y_hat))
+            mcc = matthews_corrcoef(y_true, y_hat)
+            mccs.append(mcc)
+
+        # PLOT
+        fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 4))
+
+        roc_display.plot(ax=ax1)
+        ax1.plot([0, 1], [0, 1], transform=ax1.transAxes, color='black', ls='--')
+        pr_display.plot(ax=ax2)
+        ax2.axhline(y=sum(y_true)/len(y_true), color='black', linestyle='--')
+
+        idx_max = np.argmax(mccs)
+        label = f'{thresholds[idx_max]:.2f}'
+        ax3.scatter(thresholds, mccs, s=10, label=label)
+        ax3.set_ylabel("MCC")
+        ax3.set_xlabel("Threshold")
+        ax3.legend(markerscale=0, loc='upper left', title="Best threshold")
+
+        fig.suptitle(lp, fontsize=16)
+        fig.tight_layout()
+        fig.savefig(os.path.join(results_path, f'{lp}_curves.png'), bbox_inches='tight')
+
 
 if __name__ == "__main__":
     main()
