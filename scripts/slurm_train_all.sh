cd408ef13af54dc785321fe2d248bcdf0f20913f
joannacknight <jknight@turing.ac.uk>
2024-04-11 10:07:56 +0100

    delete unnecessary files

--- scripts/slurm_train_all.sh
+++
@@ -1,9 +0,0 @@
-# This will loop through the slurm files in an experiment group and create a job for each of them
-# Usage: scripts/slurm_train_all.sh drop-only
-for FILE in find ./slurm_scripts/${1}/
-do
-  if [[$FILE == *.sh ]]
-  then
-    sbatch $FILE
-  fi
-done
