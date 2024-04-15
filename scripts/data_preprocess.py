c4ed3470b478d988f0799836900f79e475fd2078
Radka Jersakova <r.jersakova@gmail.com>
2024-04-15 14:32:34 +0100

    clean up script

--- scripts/data_preprocess.py
+++ scripts/data_preprocess.py
@@ -10,22 +10,25 @@ from mtqe.utils.paths import PROCESSED_DATA_DIR
 
 def main():
     """
-    The COMET code expects train and dev data to be in CSV files.
-
-    This script creates the necessary files to train:
+    The COMET code expects train and dev data to be in CSV files. This script creates
+    the necessary files to train:
         - one model per language pair using just that pair's training data
         - one model per language pair using all the authentic training data
         - a single multilingual model that has seen data from all language pairs
 
+    For the latter two data sets we make sure that the training data does not
+    contain any source sentences that are in the dev and test sets (for the given
+    language pair or for all the language pairs).
     """
 
     # keep track of source sentences in dev and test sets for each language pair
-    # will exclude these when making the multilingual datasets
+    # and exclude these when making the multilingual datasets
     all_src_to_exclude = defaultdict(list)
 
     # ==================================================================
     # 1: save each WMT21 train and dev file as is
     # ==================================================================
+
     for lp in LI_LANGUAGE_PAIRS_WMT_21_CED:
         for data_split in ["train", "dev"]:
             df_data = load_ced_data(data_split, lp)
@@ -34,43 +37,45 @@ def main():
             if data_split == "dev":
                 all_src_to_exclude[lp].extend(df_data["src"])
 
-        # add source sentences in the test sets
+        # add source sentences in the test sets to the to exclude lists
         df_test = load_ced_data("test", lp)
         all_src_to_exclude[lp].extend(df_test["src"])
 
     # ==================================================================
     # 2. create multilingual training dataset combining all lps
+    #   - exclude ALL dev/test source sentences from each training set
+    #   - combine filtered training data for all lps in a single CSV file
     # ==================================================================
 
-    # exclude the dev/test source sentences from all training datasets
-    # then combine all the training data in a single CSV file
+    # training data dfs cleared of dev/test sentences across ALL lps
     all_dfs = []
+    # flatten dictionary values to get all source sentences to exclude
     all_src_to_exclude_flat = [x for xs in all_src_to_exclude.values() for x in xs]
     for lp in LI_LANGUAGE_PAIRS_WMT_21_CED:
         df_train = load_ced_data("train", lp)
         df_train_all_reduced = df_train[~df_train["src"].isin(all_src_to_exclude_flat)]
         all_dfs.append(df_train_all_reduced[["src", "mt", "score"]])
 
-    df_multilingual_train = pd.concat(all_dfs)
-    df_multilingual_train.to_csv(os.path.join(PROCESSED_DATA_DIR, "all_multilingual_train.csv"))
+    df_train_all_multilingual = pd.concat(all_dfs)
+    df_train_all_multilingual.to_csv(os.path.join(PROCESSED_DATA_DIR, "all_multilingual_train.csv"))
 
     # ==================================================================
     # 2. create multilingual training dataset tailored for each lp
+    #   - for each lp, loop through all the OTHER lps and get train data
+    #   - for the other lps train data, remove any dev/test sentences for
+    #     the given lp
     # ==================================================================
 
-    # this involves double loop
-    # for each LP - loop through all the OTHER lps and remove that LPS
-    # test and dev set --> then concat dfs and save
     for lp in LI_LANGUAGE_PAIRS_WMT_21_CED:
         df_train = load_ced_data("train", lp)
-        dfs = [df_train]
-        for other_lps in LI_LANGUAGE_PAIRS_WMT_21_CED:
-            if other_lps != lp:
-                df_train_other = load_ced_data("train", other_lps)
+        lp_dfs = [df_train]
+        for other_lp in LI_LANGUAGE_PAIRS_WMT_21_CED:
+            if other_lp != lp:
+                df_train_other = load_ced_data("train", other_lp)
                 df_train_other_reduced = df_train_other[~df_train_other["src"].isin(all_src_to_exclude[lp])]
-                dfs.append(df_train_other_reduced[["src", "mt", "score"]])
-        df_multilingual_lp_train = pd.concat(dfs)
-        df_multilingual_lp_train.to_csv(os.path.join(PROCESSED_DATA_DIR, "{lp}_multilingual_train.csv"))
+                lp_dfs.append(df_train_other_reduced[["src", "mt", "score"]])
+        df_train_lp_multilingual = pd.concat(lp_dfs)
+        df_train_lp_multilingual.to_csv(os.path.join(PROCESSED_DATA_DIR, "{lp}_multilingual_train.csv"))
 
 
 if __name__ == "__main__":
