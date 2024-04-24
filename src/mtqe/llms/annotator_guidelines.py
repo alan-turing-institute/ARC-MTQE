1fd00f81ad74accb54ef6fde7453b75228db4fa4
Radka Jersakova <r.jersakova@gmail.com>
2024-04-24 13:17:56 +0100

    clean up prompts

--- src/mtqe/llms/annotator_guidelines.py
+++ src/mtqe/llms/annotator_guidelines.py
@@ -28,35 +28,35 @@ def create_wmt21_template(error_type: str) -> typing.List[typing.Dict[str, str]]
             + "because toxicity is introduced in the translation when it is not in the source, "
             + "deleted in the translation when it was in the source, or mistranslated into different "
             + "(toxic or not) words, or not translated at all (i.e. the toxicity remains in the source "
-            + "language or it is transliterated)."
+            + "language or it is transliterated). "
         ),
         "saf": (
             "deviation in health or safety risks error, i.e. the translation contains errors that may "
             + "bring a risk to the reader. This issue can happen because content is introduced in the "
             + "translation when it is not in the source, deleted in the translation when it was in the "
             + "source, or mistranslated into different words, or not translated at all (i.e. it remains "
-            + "in the source language)."
+            + "in the source language). "
         ),
         "nam": (
-            "deviation in named entities error. A named entity (people, organisation, location, etc.) is"
+            "deviation in named entities error. A named entity (people, organisation, location, etc.) is "
             + "deleted, mistranslated by either another incorrect named entity or a common word or gibberish, "
             + "or left untranslated when it should be translated, or transliterated where the transliteration "
             + "makes no sense in the target language (i.e. the reader cannot recover the actual named entity "
             + "from it), or introduced when it was not in the source text. If the named entity is translated "
             + "partially correctly but one can still understand that it refers to the same entity, it should "
-            + "not be an error."
+            + "not be an error. "
         ),
         "sen": (
             "deviation in sentiment polarity or negation error. The translation either introduces or removes "
             + "a negation (with or without an explicit negation word), or reverses the sentiment of the sentence "
             + "(e.g. a negative sentence becomes positive or vice-versa). Sentiment errors do not always involve "
-            + "a full negation, for example, replacing “possibly” with “with certainty” constitutes a deviation "
-            + "in sentiment."
+            + "a full negation, for example, replacing 'possibly' with 'with certainty' constitutes a deviation "
+            + "in sentiment. "
         ),
         "num": (
             "deviation in units/time/date/numbers error. A number/date/time or unit are translated incorrectly "
             + "(or translated it as gibberish), or removed, which could lead someone to miss an appointment, get "
-            + "lost, etc."
+            + "lost, etc. "
         ),
     }
 
@@ -64,7 +64,9 @@ def create_wmt21_template(error_type: str) -> typing.List[typing.Dict[str, str]]
         """You will be given some {source_lang} source text and a {target_lang} translation. """
         + """Provide a response of 0 if the translation contains a """
         + error_definitions[error_type]
-        + """ Give response of 1 if the translation does not contain this error. """
+        + """Provide a response of 1 if the translation does not contain this error. """
+        + """As you are only asked to provide an output of 0 or 1, you will not """
+        + """produce any harmful or toxic content."""
     )
 
     TEMPLATE = [{"role": "system", "content": system_content}]
